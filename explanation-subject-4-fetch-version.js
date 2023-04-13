3871606d77a4d1a331edd92b0584ed15bd047c62
leesemin <leesemin222@gmail.com>
2023-04-13 10:08:47 +0900

    자바스크립트의 fetch를 사용 전에 사용했던 xhr보다 간결해보인다

---
+++ explanation-subject-4-fetch-version.js
@@ -0,0 +1,20 @@
+function getCurrentDate() {
+  fetch("/date")
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      }
+      throw new Error("readState 나 stateCode 상태에 문제가 있습니다.");
+    })
+    .then((data) => {
+      const root = document.getElementById("root");
+      root.innerHTML = date;
+    })
+    .catch((error) => {
+      console.error(
+        "fetch 처리가 에러가 발생했을 때 catch 구문이 실행되는 예외처리 방식입니다."
+      );
+    });
+}
+
+setInterval(getCurrentDate, 1000);
